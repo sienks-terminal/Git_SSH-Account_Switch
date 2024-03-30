43106e6e730f78748cf6294f55a4a1cb8038696d
sienks <69677088+sienks@users.noreply.github.com>
2024-03-30 15:25:31 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,10 +21,11 @@ function Echo_Color(){
         echo -e "$COLOR$2\033[0m"
     }
 
+# zsh shell will respect XDG spec if it is set, not currently supported in bash
 case $SHELL in
     *zsh )
-    profile=~/.zshrc
-    logout_profile=~/.zlogout
+    profile=${ZDOTDIR:-$HOME}/.zshrc
+    logout_profile=${ZDOTDIR:-$HOME}/.zlogout
     ;;
     *bash )
     profile=~/.bashrc
@@ -37,28 +38,39 @@ case $SHELL in
     ;;
 esac
 
-gitacc_config='# git account switch
-source $HOME/.git-acc'
+# source git-acc.sh from the current directory, that is the directory the setup.sh script
+# is run from 
+gitacc_config="# git account switch
+source $PWD/git-acc.sh"
 
-cp ./git-acc.sh ~/.git-acc
+#cp ./git-acc.sh ~/.git-acc
 
 if [ "$profile" = "unknown" ]; then
     echo 'Paste the information down below to your profile:'
     Echo_Color y "$gitacc_config\n"
     
-    echo 'Paste the information down below to your profile:'
-    Echo_Color y "$(cat ./logout.script)\n"
+    #echo 'Paste the information down below to your profile:'
+    #Echo_Color y "$(cat ./logout.script)\n"
 else
     if [ "$(grep -xn "$gitacc_config" $profile)" != "" ]; then
         Echo_Color g "You have already added git-acc config in $profile !!\nOnly update your git-acc!"
     else
         printf "\n$gitacc_config\n" >> $profile
-        echo "$(cat ./logout.script)" >> $logout_profile
+        #echo "$(cat ./logout.script)" >> $logout_profile
     fi
 fi
 
-if ! [ -f ~/.gitacc ]; then
-    printf "" >> ~/.gitacc
+# Check if XDG_DATA_HOME is set, otherwise use the default directory
+GITACC_DIR="~"
+
+if [ -n "$XDG_DATA_HOME" ]; then
+    GITACC_DIR="$XDG_DATA_HOME"
+fi
+
+# Check if .gitacc exists
+if [ ! -f "$GITACC_DIR/.gitacc" ]; then
+    # Create .gitacc in the default directory
+    touch "$GITACC_DIR/.gitacc"
 fi
 
 source $profile
